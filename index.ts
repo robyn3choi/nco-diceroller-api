663cd20056500c008763f0017b5d7fad28d4a461
Robyn Choi <robyn3choi@gmail.com>
2023-10-10 09:33:57 -0700

    fixed player leave bug

--- index.ts
+++ index.ts
@@ -1,13 +1,11 @@
-const express = require("express")
-const app = express()
-const http = require("http")
-const cors = require("cors")
+import express from "express"
+import http from "http"
+import cors from "cors"
 import { Server } from "socket.io"
 
 const port = process.env.PORT || 3001
-
+const app = express()
 app.use(cors())
-
 const server = http.createServer(app)
 
 const io = new Server(server, {
@@ -17,20 +15,20 @@ const io = new Server(server, {
   },
 })
 
-const players: { name: string; room: string }[] = []
+const players: { socketId: string; name: string; room: string }[] = []
 
 app.get("/", (req, res) => {
   res.send("hello world")
 })
 
 io.on("connection", (socket) => {
-  console.log("connected", socket.id)
+  console.log(`Connected with socketId: ${socket.id}`)
 
   socket.on("playerJoined", ({ room, playerName }) => {
     socket.join(room)
-    players.push({ name: playerName, room })
+    players.push({ socketId: socket.id, name: playerName, room })
 
-    console.log(`Player: ${playerName} joined room: ${room}`)
+    console.log(`Player: ${playerName} with socketId: ${socket.id} joined room: ${room}`)
 
     const playersInRoom = players.filter((p) => p.room === room).map((p) => p.name)
 
@@ -56,10 +54,15 @@ io.on("connection", (socket) => {
     socket.rooms.forEach((r) => {
       if (r !== socket.id) room = r
     })
-    const index = players.findIndex((p) => p.room === room)
+
+    const index = players.findIndex((p) => p.socketId === socket.id)
     if (index > -1) {
-      players.splice(index, 1)
+      const [removedPlayer] = players.splice(index, 1)
+      console.log(`Player: ${removedPlayer.name} with socketId: ${socket.id} left room: ${room}`)
+    } else {
+      console.log(`Tried to remove player with socketId: ${socket.id} but it didn't exist in the players array`)
     }
+
     io.to(room).emit(
       "playersUpdated",
       players.map((p) => p.name)
@@ -69,7 +72,7 @@ io.on("connection", (socket) => {
 
 app.get("/player", (req, res) => {
   const { playerName, room } = req.query
-  if (players.find((p) => p.room === room && p.name === playerName)) {
+  if (players.find((p) => p.room === room && p.name.toLowerCase() === (playerName as string).toLowerCase())) {
     res.send({ exists: true })
   } else {
     res.send({ exists: false })
@@ -77,5 +80,5 @@ app.get("/player", (req, res) => {
 })
 
 server.listen(port, () => {
-  console.log(`Socket.IO server running at http://localhost:${port}/`)
+  console.log(`nco-diceroller-server running on port: ${port}`)
 })
